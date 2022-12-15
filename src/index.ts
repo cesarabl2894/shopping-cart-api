cc3276751ef79c08e7386246a87bdea013d9d85f
CesarBonel <cbonel@sanservices.hn>
2022-12-15 01:14:35 -0600

    Removing Unnecesary Libraries

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,6 @@
-import * as moduleAlias from 'module-alias';
 import * as dotenv from 'dotenv';
 dotenv.config();
 const port = process.env.PORT || 5000;
-const sourcePath = 'src';
-
-moduleAlias.addAliases({
-  '@server': sourcePath,
-  '@controllers': `${sourcePath}/controller`,
-  '@middleware': `${sourcePath}/middleware`,
-  '@utils': `${sourcePath}/utils`,
-});
 
 import { createServer } from './server';
 
